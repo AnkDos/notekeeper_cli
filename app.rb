6e487cde0d9ed72156a908a71b53ef04195b1f4d
AnkDos <ankurpan96@gmail.com>
2018-06-07 11:20:22 +0000

    note adding feature added

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'net/http'
 
 def add_note(date_of_add,due_date,subject,body)
+url=URI("http://vez.hgf.mybluehost.me/paperless_api/insertnote.php?date=#{date_of_add}&due=#{due_date}&sub=#{subject}&body=#{body}")
+insert=Net::HTTP.get(url)
 
 end
 
@@ -12,7 +14,6 @@ end
 def info()
 puts "\t Welcome to The NoteKeeper Cli"
 puts " Add notes , keep the subject short as its the uniq constraint "
-
 end
 
 
