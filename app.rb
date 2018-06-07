b34e2f096c63d74986288c40fb3e92f2b16faf97
AnkDos <ankurpan96@gmail.com>
2018-06-07 11:37:28 +0000

    added method for viewing notes

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'net/http'
+require 'pp'
 
 def add_note(date_of_add,due_date,subject,body)
 url=URI("http://vez.hgf.mybluehost.me/paperless_api/insertnote.php?date=#{date_of_add}&due=#{due_date}&sub=#{subject}&body=#{body}")
 insert=Net::HTTP.get(url)
+end
+
+def view_notes()
 
 end
 
@@ -18,8 +22,10 @@ puts " Add notes , keep the subject one word and no space as its the uniq constr
 end
 
 def options()
- 
-    
+ puts  ""   
+ puts "1. Insert a new note "
+ puts "2. View Notes"
+ puts "3. Delete Notes"
 end
 
 #user Interaction Panel 
